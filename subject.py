afead0129d86dd358db536ab9e6a6fb1a4f2b0f2
Charlie-LI <168180004+Charliecaptain@users.noreply.github.com>
2024-05-07 17:02:12 +1000

    Add files via upload

--- subject.py
+++ subject.py
@@ -1,22 +1,17 @@
 import random
 
 class Subject():
-    #def __init__(self, subject_id=None, mark=None):
-    #   if subject_id and mark:
-    #       self.subject_id = subject_id
-    #      self.mark = mark
-    #      print("No marks or id")
+    def __init__(self, subject_id=None, mark=None):
+        if subject_id and mark:
+            self.subject_id = subject_id
+            self.mark = mark
+            #print("No marks or id")
         else:
             self.subject_id = self.new_id()
             self.mark = self.new_mark()
             #print("marks and id", f"{self.mark}", f"{self.subject_id}")
-        self.grade = self.get_grade(self.mark)
+            self.grade = self.get_grade(self.mark)
 
-    def new_id(self):
-        ID =random.randint(1,999)
-        line = str(ID).rjust(3, '0')
-        print ("Your student ID is", line)
-        return ID
         '''
         ID randomly generated 1 <= ID <= 999, unique and formatted as 3-digits width
         IDs less than 3-digits width should be completed with zeroes from the left.
@@ -28,12 +23,10 @@ class Subject():
         # Example: subject_id = "023"
         #               return subject_id
         # NOTE: value types can be checked with print(type(value))
-        
-
-    def new_mark(self):
-        new_mark= random.randint(25,100)
-        return new_mark
-        #print ("Your mark is",new_mark)
+    def new_id(self):
+        ID =random.randint(1,999)
+        line = str(ID).rjust(3, '0')
+        return ID
         '''
         mark is randomly generated where 25<= mark <= 100
         '''
@@ -43,20 +36,11 @@ class Subject():
         #               return mark
         # NOTE: value types can be checked with print(type(value))
         
-
-    def get_grade(self, mark):
+    def new_mark(self):
+        new_mark= random.randint(25,100)
+        return new_mark   
         
-        if mark < 50: 
-            return "F"
-        if mark >= 50 and mark < 65: 
-            return "P"
-        if mark >= 65 and mark < 75: 
-            return "C"
-        if mark >= 75 and mark < 85: 
-            return "D"
-        if mark >= 85:
-            return "HD"
-        
+            
         '''
         grade is determined based on the mark
         '''
@@ -68,7 +52,19 @@ class Subject():
         # If mark >= 75 and mark < 85, grade = "D"
         # If mark >= 85, grade = "HD"
         
-
+    def get_grade(self, mark):
+        
+        if mark < 50: 
+            return "F"
+        if mark >= 50 and mark < 65: 
+            return "P"
+        if mark >= 65 and mark < 75: 
+            return "C"
+        if mark >= 75 and mark < 85: 
+            return "D"
+        if mark >= 85:
+            return "HD"   
+        
 def main():
     # You can test your code here
     test_subject = Subject()
