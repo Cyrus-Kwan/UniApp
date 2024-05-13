764c41e184909aef6fc22d7bdb548ec80369829c
OZON-3 <advent_123@yahoo.com>
2024-05-13 15:53:29 +1000

    Update database.py

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ class Database():
 
         # Database path, this will be used to create a Database object to perform CRUD operations on
         self.path:str = self.get_path(filepath)
-        os.makedirs(os.path.dirname(self.path) + self.separator, exist_ok=True)
+        os.makedirs(os.path.dirname(self.path) + Database.separator, exist_ok=True)
 
         # Reads the filepath into a data field and then closes the file
         # if the filepath does not exist, then it is first created and that is read into data
@@ -46,7 +46,7 @@ class Database():
         # Guard clause for None paths
         if not self.path:
             cli.error(f"'{self.path}' is an invalid path, default 'student.data' was created instead.")
-            self.path = self.separator.join((self.current_dir, self.default_name))
+            self.path = Database.separator.join((self.current_dir, self.default_name))
         
         # 'w' creates a new file for writing
         return open(self.path, mode="w")
@@ -122,8 +122,8 @@ class Database():
         re_file, = re.findall(r"^[^/\\]+$", filepath) or [None]
         re_path, = re.findall(r"^.*$", filepath) or [None]
 
-        if re_dir: return self.separator.join((re_dir, self.default_name))
-        elif re_file: return self.separator.join((self.current_dir, re_file))
+        if re_dir: return Database.separator.join((re_dir, self.default_name))
+        elif re_file: return Database.separator.join((self.current_dir, re_file))
         else: return re_path
 
 def main():
