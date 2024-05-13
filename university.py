3c658d59bbb80a98c05b1f2ed37f049850de26ac
Charlie-LI <168180004+Charliecaptain@users.noreply.github.com>
2024-05-13 18:08:08 +1000

    Add files via upload

--- university.py
+++ university.py
@@ -1,14 +1,13 @@
 from admin import Admin
 from student import Student
-from database import Database
+from database import Database as db
 import cli
 
 class University:
 
-    def __init__(self, name, database):
+    def __init__(self, name):
         self.name = name
-        Database.initialize()
-        self.student = Database.read()
+        cli.menu(self)
         
     def student(self):
         return Student()
@@ -16,17 +15,16 @@ class University:
     def admin(self):
         return Admin() 
 
-    def main():
-        uni = University("CBA")
-        cli.menu(uni)
-    '''
+def main():
+    pass
+'''
     The university menu system should enable users to choose to go the Admin menu or Student Menu
 
     (A) Admin
     (S) Student
     (X) Exit
-    '''
+'''
     
-    if __name__ == '__main__':
-        main() 
+if __name__ == '__main__':
+    main() 
     
