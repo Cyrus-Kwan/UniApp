96ce9a0475b83853316d84b82da0ade7bd6f76d8
Ayusxh06 <ayush.patel@student.uts.edu.au>
2024-05-15 07:03:23 +0530

    Update university.py

--- university.py
+++ university.py
@@ -13,19 +13,31 @@ class University():
     (X) Exit
     '''
     def __init__(self, database:Database):
+        '''
+        Initializes the University object with a database.
+
+        Args:
+        - database (Database): The database object to be used by the University.
+        '''
         self.database:Database = database
-        cli.menu(self)
+        cli.menu(self)  # Call the menu function from the cli module to display the menu options
 
     def admin(self):
+        '''
+        Creates an instance of the Admin class and returns it.
+        '''
         return Admin(self.database)
 
     def student(self):
+        '''
+        Creates an instance of the Student class and returns it.
+        '''
         return Student(self.database)
 
 def main():
-    DATAFILE = "student.data"
-    database = Database(filepath=DATAFILE)
-    University(database=database)
+    DATAFILE = "student.data" # Filepath for the student data file.
+    database = Database(filepath=DATAFILE)  # Create a Database instance with the specified filepath.
+    University(database=database)  # Create an instance of University with the database object
 
 if __name__ == "__main__":
-    main()
+    main() # Call the main function when the script is executed
