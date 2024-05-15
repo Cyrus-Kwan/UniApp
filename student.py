18145decc2daa9bfd2b86fb8da869bb9bfe96e10
OZON-3 <advent_123@yahoo.com>
2024-05-15 10:35:47 +1000

    Update student.py

--- student.py
+++ student.py
@@ -24,6 +24,8 @@ class Student():
         “students.data”. After login, a student goes to Student Course Menu that offers the choices:
     '''
 
+    properties = ["id", "name", "email", "password", "subjects", "marks"]
+    
     def __init__(self, database:Database):
         self.database = database
         cli.menu(self)
